1f915b1ca328d2d68140d06cbd839c16abef0bca
kaisermtv <kaisermtv@yahoo.com>
2018-10-11 17:31:14 +0700

    Thêm menu, dashboard component

--- AngularLogin/ClientApp/src/app/app.module.ts
+++ AngularLogin/ClientApp/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './admin/nav-menu/nav-menu.component';
+import { DanboardComponent } from './admin/danboard/danboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    DanboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: DanboardComponent, pathMatch: 'full' },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
