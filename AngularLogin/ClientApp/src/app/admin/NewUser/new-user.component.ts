8d7b011110be7180b1ba639c37dc9bd61fa4c941
kaisermtv <kaisermtv@yahoo.com>
2018-10-17 10:24:23 +0700

    - Sửa lại giao diện - Sửa lỗi form

--- AngularLogin/ClientApp/src/app/admin/NewUser/new-user.component.ts
+++ AngularLogin/ClientApp/src/app/admin/NewUser/new-user.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-new-user',
@@ -9,15 +10,34 @@ export class NewUserComponent {
   powers = ['Really Smart', 'Super Flexible',
     'Super Hot', 'Weather Changer'];
 
-  model = new UserInfo("","","");
+  //model = new UserInfo("êg","fbs","sbsd");
+  
+  model = new UserInfo('','','','');
 
   submitted = false;
+    http: any;
+    baseUrl: string;
+
+  
+  //constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+  //  this.http = http;
+  //  this.baseUrl = baseUrl;
+  //}
 
   onSubmit() {
     this.submitted = true;
-
     console.log(this.model);
+    //this.http.post
   }
+
+  newHero() {
+    this.model = new UserInfo('', '', '','');
+  }
+
+  //onSubmit() {
+
+  //  console.log(this.model);
+  //}
   
 }
 export class UserInfo {
@@ -26,6 +46,7 @@ export class UserInfo {
     public UserName: string,
     public Password: string,
     public RePassword: string,
+    public Role: string,
   ) { }
 
 }
